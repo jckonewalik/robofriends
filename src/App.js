7864c5782566eed301c8cece4459730c42ec329c
Joao Konewalk <jckonewalik@gmail.com>
2019-02-07 21:39:31 -0200

    Add scroll to the window

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import "./App.css";
 import CardList from "./CardList";
+import Scroll from "./Scroll";
 import SearchBox from "./SearchBox";
-
 class App extends Component {
   constructor() {
     super();
@@ -35,7 +35,9 @@ class App extends Component {
         <div className="tc">
           <h1 className="f1">RoboFriends</h1>
           <SearchBox searchChange={this.onSearchChange} />
-          <CardList robots={filteredRobots} />
+          <Scroll>
+            <CardList robots={filteredRobots} />
+          </Scroll>
         </div>
       );
     }
